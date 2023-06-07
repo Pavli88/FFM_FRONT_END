d0e4910eea5999622a7c175d58faad5bc7e3caa9
pavliati <pavlicsekati@gmail.com>
2023-06-07 18:05:20 +0200

    fed

--- src/Pages/CalculationsPage/CalculationOptions/CalculationOptions.js
+++ src/Pages/CalculationsPage/CalculationOptions/CalculationOptions.js
@@ -1,19 +1,29 @@
 import Card from "react-bootstrap/Card";
 import CalculationContext from "../CalculationPageContext/calculation-context";
+import DateContext from "../../../context/date-context";
 import {useContext, useRef, useState} from "react";
 import axios from "axios";
 import Select from "react-select";
 
 const CalculationOptions = (props) => {
+    const currentDate = useContext(DateContext).currentDate;
+    const startDateRef = useRef();
+    const [process, setProcess] = useState();
+    const [parameters, setParameters] = useState({'url': '',})
 
-    const runCalculation = () => {
-        console.log('run calc')
-    };
+    const startDateDiv = <div style={{paddingLeft: 15, display: "flex"}}>
+        <div style={{paddingTop: 0, width: 150}}>
+                        <span className={'input-label'} style={{textAlign: "left"}}>
+                            Start Date
+                        </span>
+        </div>
+        <input ref={startDateRef} type={"date"} defaultValue={currentDate}/>
+    </div>
 
-    return(
+    return (
         <div style={{padding: 15}}>
             <Card>
-            <div style={{display: "flex", paddingLeft: 15, paddingTop: 5, paddingBottom: 5, paddingRight: 15}}>
+                <div style={{display: "flex", paddingLeft: 15, paddingTop: 5, paddingBottom: 5, paddingRight: 15}}>
                     <div style={{paddingTop: 0}}>
                         <span className={'input-label'} style={{textAlign: "left"}}>
                             Process
@@ -22,20 +32,28 @@ const CalculationOptions = (props) => {
                     <div style={{paddingLeft: 15, paddingTop: 0, width: 200}}>
                         <Select
                             options={[
-                                {value: 'Valuation', label: 'Valuation'}
+                                {value: 'valuation', label: 'Valuation'},
+                                {value: 'total_return', label: 'Total Return'},
+                                {value: 'attribution', label: 'Attribution'}
                             ]}
                             isClearable
-                            // onChange={(e) => e === null ? setSelectedGroup([]) : setSelectedGroup(e)}
+                            onChange={(e) => setProcess(e.value)}
                             className={'instrument-search-input-field'}
 
                         />
                     </div>
 
+                    {process === 'valuation' ? startDateDiv: ''}
+
                     <div style={{paddingLeft: 10, paddingTop: 0, paddingBottom: 0}}>
-                        <button className={'get-button'} onClick={runCalculation}>Run</button>
+                        <button className={'get-button'} onClick={() => props.run({
+                            'url': 'calculate/valuation/',
+                            params: {'date': startDateRef.current.value}
+                        })}>Run
+                        </button>
                     </div>
                 </div>
-        </Card>
+            </Card>
         </div>
 
     );
--- src/Pages/CalculationsPage/CalculationsPage.js
+++ src/Pages/CalculationsPage/CalculationsPage.js
@@ -1,5 +1,6 @@
 import CalculationOptions from "./CalculationOptions/CalculationOptions";
 import CalculationPortfoliosTable from "./CalculationPortfoliosTable/CalculationPortfoliosTable";
+import CalculationResponseTable from "./CalculationResponseTable/CalculationResponseTable";
 import ServerContext from "../../context/server-context";
 import CalculationContext from "./CalculationPageContext/calculation-context";
 import DateContext from "../../context/date-context";
@@ -10,10 +11,9 @@ const CalculationsPage = (props) => {
     const server = useContext(ServerContext)['server'];
     const [startDate, setStartDate] = useState(useContext(DateContext)['currentDate']);
     const [endDate, setEndDate] = useState(useContext(DateContext)['currentDate']);
-    const [selectedDate, setSelectedDate] = useState(useContext(DateContext)['currentDate']);
     const [selectedPortfolios, setSelectedPortfolios] = useState([]);
-    const [calcDate, setCalDate] = useState(useContext(DateContext)['currentDate']);
     const [portfolios, setPortfolios] = useState([{}]);
+    const [calcResponse, setCalcResponse] = useState([{}]);
 
     const fetchPortfolios = async() => {
         const response = await axios.get(server + 'portfolios/get/portfolios/'
@@ -25,28 +25,32 @@ const CalculationsPage = (props) => {
         fetchPortfolios()
     }, [])
 
+    const runCalculation = async(parameters) => {
+        const response = await axios.post(server + parameters.url, {...parameters.params,
+            portfolios: selectedPortfolios
+            }
+        )
+        setCalcResponse(response.data)
+    };
+
     return (
         <CalculationContext.Provider value={{
             selectedPortfolios: selectedPortfolios,
             saveSelectedPortfolios: setSelectedPortfolios,
-            currentDate: calcDate,
-            saveCalcDate: setCalDate,
             startDate: startDate,
             saveStartDate: setStartDate,
             endDate: endDate,
             saveEndDate: setEndDate,
-            selectedDate: selectedDate,
-            saveSelectedDate: setSelectedDate,
         }}>
             <div className={'page-container'}>
-                <CalculationOptions/>
-                <div>
+                <CalculationOptions run={runCalculation}/>
+                <div style={{display: "flex"}}>
                     <div style={{paddingBottom: 15, paddingLeft: 15, paddingRight: 15, width: '30%', height: '100%'}}>
-                        <CalculationPortfoliosTable data={portfolios}/>
+                        <CalculationPortfoliosTable data={portfolios} />
                     </div>
 
-                    <div>
-
+                    <div style={{paddingBottom: 15, paddingRight: 15, width: '70%', height: '100%'}}>
+                        <CalculationResponseTable data={calcResponse}/>
                     </div>
                 </div>
             </div>
