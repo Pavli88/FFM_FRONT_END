dc3adbff83e9110df3c5cf7424d5ff5d780bec62
pavliati <pavlicsekati@gmail.com>
2023-12-06 17:10:50 +0100

    fed

--- src/Pages/PortfolioPage/SubPages/PortfolioRisk/PositionExposure/ExposureHolding.js
+++ src/Pages/PortfolioPage/SubPages/PortfolioRisk/PositionExposure/ExposureHolding.js
@@ -3,8 +3,9 @@ import {useExpanded, useGroupBy, useTable} from "react-table";
 import Card from "react-bootstrap/Card";
 import {BsCaretDownFill, BsCaretUpFill, BsDashSquare, BsPlusSquare} from "react-icons/bs";
 
+
 const ExposureHolding = (props) => {
-    const [contributions, setContributions] = useState([]);
+    const [rowList, setRowList] = useState([]);
     const data = useMemo(
         () => props.data,
         [props.data]
@@ -16,6 +17,48 @@ const ExposureHolding = (props) => {
                 Header: 'Name',
                 accessor: 'instrument_name',
 
+            },
+            {
+                Header: 'End Pos',
+                accessor: 'ending_pos',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`
+            },
+            {
+                Header: 'P&L',
+                accessor: 'unrealized_pnl',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
+            },
+             {
+                Header: 'Simulated P&L',
+                accessor: 'sim_profit',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
+            },
+            {
+                Header: 'Contribution %',
+                accessor: 'contribution',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
+            },
+             {
+                Header: 'Simulated Contr %',
+                accessor: 'sim_contr',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
+            },
+            {
+                Header: 'Sensitivity',
+                accessor: 'sensitivity',
+                aggregate: 'sum',
+                disableGroupBy: true,
+                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
             },
             {
                 Header: 'Currency',
@@ -31,42 +74,19 @@ const ExposureHolding = (props) => {
                 disableGroupBy: true,
             },
             {
-                Header: 'End Pos',
-                accessor: 'ending_pos',
-                aggregate: 'sum',
-                disableGroupBy: true,
-                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`
-            },
-            {
                 Header: 'Trade Price',
                 accessor: 'trade_price',
                 disableGroupBy: true,
             },
-            {
-                Header: 'FX Rate',
-                accessor: 'fx_rate',
+           {
+                Header: 'Current Price',
+                accessor: 'valuation_price',
                 disableGroupBy: true,
             },
-            {
-                Header: 'End MV',
-                accessor: 'ending_mv',
-                aggregate: 'sum',
+           {
+                Header: 'Simulated Price',
+                accessor: 'sim_price',
                 disableGroupBy: true,
-                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
-            },
-            {
-                Header: 'Unrealized P&L',
-                accessor: 'unrealized_pnl',
-                aggregate: 'sum',
-                disableGroupBy: true,
-                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
-            },
-             {
-                Header: 'Contribution %',
-                accessor: 'contribution',
-                aggregate: 'sum',
-                disableGroupBy: true,
-                Aggregated: ({ value }) => `${Math.round(value * 100) / 100}`,
             },
         ],
         []
@@ -86,23 +106,98 @@ const ExposureHolding = (props) => {
     } = tableInstance
     const firstPageRows = rows.slice(0, 200)
 
-    useEffect(() => {
-        setGroupBy(['instrument_name'])
+    const getContribValues = () => {
         const contribElements = Array.from(
             document.getElementsByClassName('agg-contribution')
+        );
+        const simContribElements = Array.from(
+            document.getElementsByClassName('sim-contrib')
+        );
+        const sensitiveElements = Array.from(
+            document.getElementsByClassName('sensitivity')
         );
         const nameElements = Array.from(
             document.getElementsByClassName('inst-name')
         );
         const contribValues = contribElements.map(data => parseFloat(data.innerHTML))
+        const simContribs = simContribElements.map(data => parseFloat(data.innerHTML))
+        const sensitivity = sensitiveElements.map(data => parseFloat(data.innerHTML))
         const nameValues = nameElements.map(data => data.textContent)
-        props.contribs({'contribs': contribValues, 'names': nameValues})
+
+        props.contribs({
+            'contribs': contribValues,
+            'names': nameValues,
+            'sim_contribs': simContribs,
+            'sensitivity': sensitivity
+        })
+    };
+
+    useEffect(() => {
+        setGroupBy(['instrument_name']);
+        getContribValues();
     }, [props.data])
 
-
-    const tableRow = (row) => {
-
+    const amend = (tranId) => {
+        props.amend(tranId)
     };
+
+    const TableRow = (props) => {
+        const [rowTicked, setRowTicked] = useState(false)
+        prepareRow(props.row)
+        // console.log(props.row)
+        return (
+            <tr {...props.row.getRowProps()}
+                style={{
+                    cursor: props.row.isGrouped ? '' : 'pointer',
+                    // background: row.isGrouped ? '#f2f4f4' : 'white'
+                }}
+            >
+                {props.row.isGrouped ? <div {...props.row.getToggleRowExpandedProps()} style={{padding: 5, height: '100%'}}>
+                            {props.row.isExpanded ? <BsCaretUpFill/> : <BsCaretDownFill/>}
+                          </div> :
+                <td>
+                    <input type={'checkbox'} onClick={() => amend(props.row.original.transaction_id)} checked={props.row.original.ticked}/>
+                </td>}
+
+                {props.row.cells.map(cell => {
+                        return (
+                            <td
+                                {...cell.getCellProps()}
+                                style={{
+                                    fontWeight: cell.isGrouped
+                                        ? "bold" : cell.isAggregated ? 'bold'
+                                            : cell.isPlaceholder
+                                                ? '#ff000042'
+                                                : 'white',
+                                    color: (cell.column.Header === 'Unrealized P&L' || cell.column.Header === 'Contribution %') && cell.value < 0 ? 'red' : (cell.column.Header === 'Unrealized P&L' || cell.column.Header === 'Contribution %') && cell.value > 0 ? 'green' : 'black',
+                                }}
+                                className={cell.isAggregated > 0 && cell.column.id === 'contribution' ? 'agg-contribution' : cell.isGrouped && cell.column.id === 'instrument_name' ? 'inst-name' : cell.isAggregated > 0 && cell.column.id === 'sim_contr' ? 'sim-contrib': cell.isAggregated > 0 && cell.column.id === 'sensitivity' ? 'sensitivity': ''}
+                            >
+                                {cell.isGrouped ? (
+                                    // If it's a grouped cell, add an expander and row count
+                                    <>
+
+                                        {cell.render('Cell')} ({props.row.subRows.length})
+                                    </>
+                                ) : cell.isAggregated ? (
+                                    // If the cell is aggregated, use the Aggregated
+                                    // renderer for cell
+                                    cell.render('Aggregated')
+                                ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
+                                    // Otherwise, just render the regular cell
+                                    cell.render('Cell')
+
+                                )}
+                            </td>
+                        )
+                    }
+                )
+                }
+            </tr>
+        )
+    };
+
+    const tableRow = firstPageRows.map((row) => <TableRow row={row}/>)
 
     return (
         <div style={{height: '100%', overflowY: 'scroll'}}>
@@ -128,71 +223,7 @@ const ExposureHolding = (props) => {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                {firstPageRows.map((row, i) => {
-                    prepareRow(row)
-                    // console.log(row)
-                    return (
-                        <tr {...row.getRowProps()}
-                            style={{
-                                cursor: row.isGrouped ? '' : 'pointer',
-                                // background: row.isGrouped ? '#f2f4f4' : 'white'
-                            }}
-
-                            // onDoubleClick={() => {
-                            //     if (row.isGrouped) {
-                            //         console.log('grouped')
-                            //     } else {
-                            //         setShowModal(true)
-                            //         setSelectedTransaction(row.original)
-                            //     }
-                            // }
-                            // }
-                        >
-                            <td>
-                                <input type={'checkbox'}/>
-                            </td>
-
-                            {row.cells.map(cell => {
-
-                                return (
-                                    <td
-                                        {...cell.getCellProps()}
-                                        style={{
-                                            fontWeight: cell.isGrouped
-                                                ? "bold"
-                                                : cell.isAggregated
-                                                    ? "bold"
-                                                    : cell.isPlaceholder
-                                                        ? '#ff000042'
-                                                        : 'white',
-                                            color: (cell.column.Header === 'Unrealized P&L' || cell.column.Header === 'Contribution %') && cell.value < 0 ? 'red' : (cell.column.Header === 'Unrealized P&L' || cell.column.Header === 'Contribution %') && cell.value > 0 ? 'green' : 'black',
-                                        }}
-                                        className={cell.isAggregated > 0 && cell.column.id === 'contribution' ? 'agg-contribution': cell.isGrouped && cell.column.id === 'instrument_name' ? 'inst-name': ''}
-                                    >
-                                        {cell.isGrouped ? (
-                                            // If it's a grouped cell, add an expander and row count
-                                            <>
-                          <span {...row.getToggleRowExpandedProps()}>
-                            {row.isExpanded ? <BsCaretUpFill/> : <BsCaretDownFill/>}
-                          </span>{' '}
-                                                {cell.render('Cell')} ({row.subRows.length})
-                                            </>
-                                        ) : cell.isAggregated ? (
-                                            // If the cell is aggregated, use the Aggregated
-                                            // renderer for cell
-                                            cell.render('Aggregated')
-                                        ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
-                                            // Otherwise, just render the regular cell
-                                            cell.render('Cell')
-
-                                        )}
-                                    </td>
-                                )
-                            })
-                            }
-                        </tr>
-                    )
-                })}
+                {tableRow}
                 </tbody>
             </table>
         </div>
--- src/Pages/PortfolioPage/SubPages/PortfolioRisk/PositionExposure/PositionExposure.js
+++ src/Pages/PortfolioPage/SubPages/PortfolioRisk/PositionExposure/PositionExposure.js
@@ -2,7 +2,7 @@ import Card from "react-bootstrap/Card";
 import Chart from "react-apexcharts";
 import ExposureHolding from "./ExposureHolding";
 import axios from "axios";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import { BsArrowRepeat } from "react-icons/bs";
 import PortfolioPageContext from "../../../context/portfolio-page-context";
 import DateContext from "../../../../../context/date-context";
@@ -12,18 +12,25 @@ const PositionExposure = (props) => {
     const portfoliCode = useContext(PortfolioPageContext).portfolioCode;
     const currentDate = useContext(DateContext).currentDate;
     const [holdingData, setHoldingdata] = useState([{}])
+    const [updatedHoldingData, setUpdatedHoldingdata] = useState([{}])
     const [nav, setNav] = useState(0.0)
+    const [simNav, setSimNav] = useState(0.0)
+    const [simDD, setSimDD] = useState(0.0)
     const [currentContribs, setCurrentContribs] = useState([]);
+    const stressRef = useRef();
 
     const fetchHoldingData = async() => {
         const response = await axios.get(props.server + 'portfolios/get/exposures/', {
             params: {
                 date: currentDate,
-                portfolio_code: portfoliCode
+                portfolio_code: portfoliCode,
+                stress_percentage: stressRef.current.value
             }
         })
         setHoldingdata(response.data.data)
         setNav(response.data.nav)
+        setSimNav(response.data.sim_nav)
+        setSimDD(response.data.sim_dd)
     };
 
     useEffect(() => {
@@ -79,27 +86,110 @@ const PositionExposure = (props) => {
             fill: {
                 opacity: 1
             },
-            legend: {
-                position: 'right',
-                offsetY: 40
-            },
+            // legend: {
+            //     position: 'right',
+            //     offsetY: 40
+            // },
         },
         series: [
             {
                 name: 'Current',
                 data: currentContribs['contribs'],
             },
+            {
+                name: 'Simulated',
+                data: currentContribs['sim_contribs'],
+            },
+        ]
+    };
+
+    const y = {
+        options: {
+            chart: {
+                toolbar: false,
+                stacked: false,
+                type: 'bar',
+            },
+            xaxis: {
+                categories: currentContribs['names'],
+                type: 'date',
+                labels: {show: true},
+                axisBorder: {
+                    show: false,
+                    color: '#78909C',
+                    height: 1,
+                    width: '100%',
+                    offsetX: 0,
+                    offsetY: 0
+                },
+                axisTicks: {
+                    show: false,
+                    borderType: 'solid',
+                    color: '#78909C',
+                    height: 6,
+                    offsetX: 0,
+                    offsetY: 0
+                },
+            },
+            yaxis: [
+                {
+                    labels: {
+                        formatter: function (val) {
+                            return val.toFixed(2);
+                        }
+                    },
+                },
+            ],
+            dataLabels: {
+                enabled: false
+            },
+            fill: {
+                opacity: 1
+            },
+            // legend: {
+            //     position: 'right',
+            //     offsetY: 40
+            // },
+        },
+        series: [
+            {
+                name: 'Current',
+                data: currentContribs['sensitivity'],
+            },
         ]
     }
 
+    const amendRowStatus = (tranID) => {
+        // console.log(tranID)
+        const updatedData = holdingData.map((data) => {
+
+            if (data['transaction_id'] === tranID) {
+                console.log(data)
+                if (data['ticked'] === false) {
+                    data['sim_contr'] = 0.0
+                }else{
+                    console.log(data['contribution'])
+                    data['sim_contr'] = 2
+                };
+                data['ticked'] = !data['ticked']
+                // console.log(data)
+                return data
+            } else {
+                return data
+            }
+            ;
+        })
+        setUpdatedHoldingdata(updatedData)
+    };
+    console.log(updatedHoldingData.length)
     return (
         <div style={{display: 'flex', height: 400, paddingBottom: 15}}>
-            <div style={{height: '100%', width: '70%', margin: '0px', paddingRight: 15}}>
+            <div style={{height: '100%', width: '60%', margin: '0px', paddingRight: 15}}>
                  <Card className="card" style={{height: '100%'}}>
                 <Card.Header>
                     <div style={{display: 'flex'}}>
                         <div>
-                            Exposure by Instrument
+                            Exposure
                         </div>
                         <div>
                             <CSVLink data={holdingData} style={{paddingLeft: 15}}>Download</CSVLink>
@@ -109,25 +199,37 @@ const PositionExposure = (props) => {
                             <div style={{paddingRight: 15}}>
                                 Holding NAV
                             </div>
-                            <div style={{paddingRight: 15}}>
+                            <div style={{paddingRight: 15, color: nav > 0.0 ? 'green': 'red'}}>
                                 {nav}
                             </div>
                             <div style={{paddingRight: 15}}>
-                                Market Movement %
+                                Simulated NAV
+                            </div>
+                            <div style={{paddingRight: 15, color: simNav > 0.0 ? 'green': 'red'}}>
+                                {simNav}
+                            </div>
+                            <div style={{paddingRight: 15}}>
+                                Drawdown
+                            </div>
+                            <div style={{paddingRight: 15, color: simDD > 0.0 ? 'green': 'red'}}>
+                                {simDD} %
+                            </div>
+                            <div style={{paddingRight: 15}}>
+                                Stress %
                             </div>
                             <div>
-                                <input style={{padding: 0, width: 60}} type={'number'} defaultValue={0.0} min={1.0}
+                                <input ref={stressRef} style={{padding: 0, width: 50}} type={'number'} defaultValue={1.0} min={1.0}
                                        step={0.5}/>
                             </div>
                             <div style={{paddingLeft: 15}}>
-                                <button className={'get-button'}><BsArrowRepeat></BsArrowRepeat></button>
+                                <button className={'get-button'} onClick={() => fetchHoldingData()}><BsArrowRepeat></BsArrowRepeat></button>
                             </div>
                         </div>
 
                     </div>
                 </Card.Header>
-                <ExposureHolding data={holdingData} contribs={setContribs}/>
-            </Card>
+                <ExposureHolding data={updatedHoldingData.length > 1 ? updatedHoldingData: holdingData} contribs={setContribs} amend={amendRowStatus}/>
+                 </Card>
             </div>
 
             <Card className="card" style={{height: '100%', width: '30%', margin: '0px'}}>
@@ -139,14 +241,35 @@ const PositionExposure = (props) => {
                     </div>
                 </Card.Header>
                 <div style={{height: '100%'}}>
-                <Chart
-                    options={x.options}
-                    series={x.series}
-                    type={'bar'}
-                    width="100%"
-                    height="100%"/>
-            </div>
+                    <Chart
+                        options={x.options}
+                        series={x.series}
+                        type={'bar'}
+                        width="100%"
+                        height="100%"/>
+                </div>
             </Card>
+
+            <div style={{height: '100%', width: '30%', margin: '0px', paddingLeft: 15}}>
+                <Card className="card" style={{height: '100%'}}>
+                    <Card.Header>
+                        <div style={{display: 'flex'}}>
+                            <div>
+                                Sensitivity
+                            </div>
+                        </div>
+                    </Card.Header>
+                    <div style={{height: '100%'}}>
+                        <Chart
+                            options={y.options}
+                            series={y.series}
+                            type={'bar'}
+                            width="100%"
+                            height="100%"/>
+                    </div>
+                </Card>
+            </div>
+
         </div>
     )
 };
