a09cdeb8fd4f2e11eccb3aa2e0047048fa47cde6
pavliati <pavlicsekati@gmail.com>
2024-10-29 14:45:19 +0100

    reworking codes and folder structures

--- src/config files/app-config.js
+++ src/config files/app-config.js
@@ -3,10 +3,10 @@ const date = new Date();
 const currentYear = new Date().getFullYear();
 // 'http://127.0.0.0:8001/' 'https://pavliati.pythonanywhere.com/'
 
-// 'http://137.184.111.7:80/'
+// 'https://137.184.111.7:80/'
 //
 const appConfig = {
-    server: 'http://137.184.111.7:80/',
+    server: 'https://137.184.111.7:80/',
     // defaultRobotEnvironment: 'live',
     currentDate: date.toISOString().substr(0,10),
     fistDayOfCurrentYear: currentYear.toString( ) + '-01-01'
