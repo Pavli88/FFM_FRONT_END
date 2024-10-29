adcb0c9b760b7e4c25e057d7e74cb5bdcc577b61
pavliati <pavlicsekati@gmail.com>
2024-10-29 15:19:55 +0100

    domain deployment

--- src/config files/app-config.js
+++ src/config files/app-config.js
@@ -2,11 +2,11 @@
 const date = new Date();
 const currentYear = new Date().getFullYear();
 // 'http://127.0.0.0:8001/' 'https://pavliati.pythonanywhere.com/'
-
+//https://fractalportfolios.com/
 // 'https://137.184.111.7:80/'
 //
 const appConfig = {
-    server: 'https://137.184.111.7/',
+    server: 'https://fractalportfolios.com/',
     // defaultRobotEnvironment: 'live',
     currentDate: date.toISOString().substr(0,10),
     fistDayOfCurrentYear: currentYear.toString( ) + '-01-01'
