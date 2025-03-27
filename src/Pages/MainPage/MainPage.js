9723032753893425915f000f1f219b92fec32578
Barbara <bbarbara092@gmail.com>
2025-03-27 12:47:21 +0100

    Modify names

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -30,7 +30,7 @@ const MainPage = (props) => {
                                 setAuthFormType("signup");
                                 setShowModal(true);
                             }}>
-                            Create Account
+                            Sign Up
                         </button>
                     </div>
                     <div style={{ margin: 5, height: 40 }}>
@@ -39,7 +39,7 @@ const MainPage = (props) => {
                                 setAuthFormType("login");
                                 setShowModal(true);
                             }}>
-                            Login
+                            Sign In
                         </button>
                     </div>
                 </div>
